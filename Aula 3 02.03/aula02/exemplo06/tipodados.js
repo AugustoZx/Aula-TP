ed0f75d9d2d8a18194a9053e910314b75f3caa74
AugustoZx <augustooliiveira@hotmail.com>
2023-03-16 09:16:06 -0300

    arquivos novos

---
+++ Aula 3 02.03/aula02/exemplo06/tipodados.js
@@ -0,0 +1,9 @@
+var nome = "Augusto"
+console.log(nome)
+
+var idade = 18
+console.log(idade)
+
+var salvo = true
+console.log(salvo)
+
---
+++ Aula 4 09.03/exemplo09/exemplo09.js
@@ -0,0 +1,13 @@
+//erro
+/*var nome = "Augusto"
+
+if (nome == "Augusto"){
+    let idade = 18
+}
+
+console.log(idade)*/
+
+/*var nome = "Augusto"
+var nome = "Joao"
+
+console.log(nome)*/
